baa0d39023806238a5c3f379d8690563132f0c2b
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-02 15:45:43 +0200

    [UPD]Added Cicle While in script.js and finished the excercise

--- js/script.js
+++ js/script.js
@@ -2,16 +2,31 @@
 
 const ingredienti = ["Uova", "Pane","Riso","Cioccolato","Gelato","Piadine","Prosciutto Cotto","Sottilette","Insalata","Funghi"]
 
-const lista_spesa = document.getElementById("lista-spesa")
-
 //Recupero l'elemento che deve contenere la spesa
+
+const lista_spesa = document.getElementById("lista-spesa")
 
 //dichiaro una variabile i = 0
 
+let i = 0
+
 //4.> Ciclo l'array con CICLE WHILE i < ingrendienti.length
 
+while(i < ingredienti.length){
+    //Creo un nuovo list item
+
+    let li = document.createElement("li")
+
+    //Inserisco il testo all'interno li = [i]
+
+    li.innerHTML = ingredienti[i]
+
+    //APPEND list item all'interno
+
+    lista_spesa.append(li)
+    i++
+
+}
 
 
-//Creo un nuovo list item
-//Inserisco il testo all'interno li = [i]
-//APPEND list item all'interno
+
