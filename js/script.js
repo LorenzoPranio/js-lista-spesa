acba3b680a9b7e963c1808932fbff9d0ffde0a6c
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-02 15:39:58 +0200

    [UPD]Added ARRAY in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 //Creare una lista della spesa (ingredienti) e la mostro a schermo 
 
+const ingredienti = ["Uova", "Pane","Riso","Cioccolato","Gelato","Piadine","Prosciutto Cotto","Sottilette","Insalata","Funghi"]
+
+const lista_spesa = document.getElementById("lista-spesa")
+
 //Recupero l'elemento che deve contenere la spesa
 
 //dichiaro una variabile i = 0
