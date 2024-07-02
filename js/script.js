fe979cf42f2a00ad892abf606b480016cc5f4d90
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-02 15:29:30 +0200

    [UPD]Added scaffolding in script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Creare una lista della spesa (ingredienti) e la mostro a schermo 
+
+//Recupero l'elemento che deve contenere la spesa
+
+//dichiaro una variabile i = 0
+
+//4.> Ciclo l'array con CICLE WHILE i < ingrendienti.length
+
+
+
+//Creo un nuovo list item
+//Inserisco il testo all'interno li = [i]
+//APPEND list item all'interno
